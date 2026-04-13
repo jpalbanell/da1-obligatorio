355cf9c06b6c3379a195124e9eb8f2c58324033a
Leandro <lc286847@fi365.ort.edu.uy>
2026-04-13 15:34:30 -0300

    feat: agregar validación de nombre vacío en Equipo

---
+++ oblDa1/Dominio/Equipo.cs
@@ -0,0 +1,18 @@
+﻿namespace Dominio
+{
+    public class Equipo
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("El nombre es obligatorio.");
+                _nombre = value;
+            }
+        }
+    }
+}
---
+++ oblDa1/Tests/EquipoTests.cs
@@ -0,0 +1,25 @@
+﻿using Dominio;
+
+namespace Tests
+{
+    [TestClass]
+    public class EquipoTests
+    {
+        [TestMethod]
+        public void CrearEquipo_ConNombreValido_DeberiaAsignarNombre()
+        {
+            var equipo = new Equipo();
+            equipo.Nombre = "Uruguay";
+            Assert.AreEqual("Uruguay", equipo.Nombre);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CrearEquipo_ConNombreVacio_DeberiaLanzarExcepcion()
+        {
+            var equipo = new Equipo();
+            equipo.Nombre = "";
+        }
+
+    }
+}
