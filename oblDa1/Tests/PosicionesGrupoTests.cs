d5ed666236f7e0a10d0156318378e0aac73e6c0f
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-04-14 17:53:39 -0300

    feat: validar GolesFavor negativos en PosicionesGrupo

--- oblDa1/Tests/PosicionesGrupoTests.cs
+++ oblDa1/Tests/PosicionesGrupoTests.cs
@@ -109,4 +109,12 @@ public class PosicionesGrupoTests
         var posicion = new PosicionesGrupo(1);
         posicion.Puntos = -1;
     }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CrearPosicionesGrupo_ConGolesFavorNegativos_DeberiaLanzarExcepcion()
+    {
+        var posicion = new PosicionesGrupo(1);
+        posicion.GolesFavor = -1;
+    }
 }
