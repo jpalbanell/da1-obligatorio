3e4a85ad3df5ede88757b1683b9173f9af0e2435
Leandro <lc286847@fi365.ort.edu.uy>
2026-04-14 12:06:15 -0300

    tefeatverificar asignación de etiqueta válida en Grupo

---
+++ oblDa1/Tests/GrupoTests.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Entidades;
+
+namespace Tests
+{
+    [TestClass]
+    public class GrupoTests
+    {
+        [TestMethod]
+        public void CrearGrupo_ConEtiquetaValida_DeberiaAsignarEtiqueta()
+        {
+            var grupo = new Grupo();
+            grupo.Etiqueta = "A";
+            Assert.AreEqual("A", grupo.Etiqueta);
+        }
+    }
+}
