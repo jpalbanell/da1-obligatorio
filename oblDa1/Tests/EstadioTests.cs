f428993eb07c15b59e779b26c8004424a455b563
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-04-14 10:17:23 -0300

    refactor: organizar entidades del dominio en carpeta Entidades

--- oblDa1/Tests/EstadioTests.cs
+++ oblDa1/Tests/EstadioTests.cs
@@ -1,5 +1,5 @@
 using System;
-using Dominio;
+using Dominio.Entidades;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests;
--- oblDa1/Tests/PartidoTests.cs
+++ oblDa1/Tests/PartidoTests.cs
@@ -1,4 +1,4 @@
-using Dominio;
+using Dominio.Entidades;
 
 namespace Tests
 {
