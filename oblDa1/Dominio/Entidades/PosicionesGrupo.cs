c73f444651075ed3f006955176d8f9bcee945f4c
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-04-14 17:55:52 -0300

    refactor: extraer métodos privados de validación en PosicionesGrupo

--- oblDa1/Dominio/Entidades/PosicionesGrupo.cs
+++ oblDa1/Dominio/Entidades/PosicionesGrupo.cs
@@ -2,85 +2,97 @@
 {
     public class PosicionesGrupo
     {
-        public int Id { get; set; }
-
-        public PosicionesGrupo(int id)
-        {
-            Id = id;
-        }
-        
+        private int _puntos;
+        private int _golesFavor;
+        private int _golesContra;
+        private int _posicionFinal;
         private Equipo _equipo;
+        private Grupo _grupo;
+
+        public int Id { get; set; }
+        public int DiferenciaGoles { get; set; }
 
         public Equipo Equipo
         {
             get => _equipo;
             set
             {
-                if (value == null) throw new ArgumentException("Equipo no puede ser nulo");
+                ValidarNoNulo(value, "Equipo");
                 _equipo = value;
             }
         }
-        
-        private Grupo _grupo;
 
         public Grupo Grupo
         {
             get => _grupo;
             set
             {
-                if (value == null) throw new ArgumentException("Grupo no puede ser nulo");
+                ValidarNoNulo(value, "Grupo");
                 _grupo = value;
             }
         }
-        
-        private int _puntos;
 
         public int Puntos
         {
             get => _puntos;
             set
             {
-                if (value < 0) throw new ArgumentException("Puntos no puede ser negativo");
+                ValidarNoNegativo(value, "Puntos");
                 _puntos = value;
             }
         }
-        
-        private int _golesFavor;
 
         public int GolesFavor
         {
             get => _golesFavor;
             set
             {
-                if (value < 0) throw new ArgumentException("GolesFavor no puede ser negativo");
+                ValidarNoNegativo(value, "GolesFavor");
                 _golesFavor = value;
             }
         }
-        
-        private int _golesContra;
 
         public int GolesContra
         {
             get => _golesContra;
             set
             {
-                if (value < 0) throw new ArgumentException("GolesContra no puede ser negativo");
+                ValidarNoNegativo(value, "GolesContra");
                 _golesContra = value;
             }
         }
-        
-        public int DiferenciaGoles { get; set; }
-        
-        private int _posicionFinal;
 
         public int PosicionFinal
         {
             get => _posicionFinal;
             set
             {
-                if (value < 1 || value > 4) throw new ArgumentException("PosicionFinal debe estar entre 1 y 4");
+                ValidarPosicionFinal(value);
                 _posicionFinal = value;
             }
+        }
+
+        public PosicionesGrupo(int id)
+        {
+            Id = id;
+        }
+
+        private void ValidarNoNulo(object valor, string campo)
+        {
+            if (valor == null)
+                throw new ArgumentException($"{campo} no puede ser nulo");
+        }
+
+        private void ValidarNoNegativo(int valor, string campo)
+        {
+            if (valor < 0)
+                throw new ArgumentException($"{campo} no puede ser negativo");
+        }
+
+        private void ValidarPosicionFinal(int valor)
+        {
+            if (valor < 1 || valor > 4)
+                throw new ArgumentException("PosicionFinal debe estar entre 1 y 4");
         }
     }
 }
