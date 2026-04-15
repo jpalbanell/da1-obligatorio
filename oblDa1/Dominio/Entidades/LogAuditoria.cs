f2364de5a85d408b3d9e28787814c971dbd207e1
JuanpeTDB <albanelljuanpedro@gmail.com>
2026-04-15 16:35:46 -0300

    feat: agregar propiedad Timestamp con validación en LogAuditoria

--- oblDa1/Dominio/Entidades/LogAuditoria.cs
+++ oblDa1/Dominio/Entidades/LogAuditoria.cs
@@ -3,5 +3,22 @@ namespace Dominio.Entidades
     public class LogAuditoria
     {
         public int Id { get; set; }
+        private DateTime _timestamp;
+
+        public DateTime Timestamp
+        {
+            get => _timestamp;
+            set
+            {
+                ValidarTimestamp(value);
+                _timestamp = value;
+            }
+        }
+
+        private void ValidarTimestamp(DateTime timestamp)
+        {
+            if (timestamp == default)
+                throw new ArgumentException("El timestamp es obligatorio.");
+        }
     }
 }
--- oblDa1/Tests/LogAuditoriaTests.cs
+++ oblDa1/Tests/LogAuditoriaTests.cs
@@ -12,5 +12,14 @@ namespace Tests
             log.Id = 1;
             Assert.AreEqual(1, log.Id);
         }
+        
+        [TestMethod]
+        public void CrearLogAuditoria_ConTimestampValido_DeberiaAsignarTimestamp()
+        {
+            var log = new LogAuditoria();
+            var fecha = new DateTime(2026, 4, 15, 10, 30, 0);
+            log.Timestamp = fecha;
+            Assert.AreEqual(fecha, log.Timestamp);
+        }
     }
 }
