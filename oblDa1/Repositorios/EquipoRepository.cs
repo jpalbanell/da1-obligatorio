cdda796a6fb5acca1363ce54daaeb0cbfb54abc9
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-04-16 15:03:53 -0300

    refactor: extraer métodos privados de validación en EquipoRepository

--- oblDa1/Repositorios/EquipoRepository.cs
+++ oblDa1/Repositorios/EquipoRepository.cs
@@ -24,17 +24,27 @@ namespace Repositorios
         public void Update(Equipo equipo)
         {
             var index = _equipos.FindIndex(e => e == equipo);
-            if (index == -1)
-                throw new Exception("Equipo no encontrado");
+            ValidarEquipoExistente(index);
             _equipos[index] = equipo;
         }
-        
+
         public void Delete(string nombre)
         {
             var equipo = GetByNombre(nombre);
+            ValidarEquipoNoNulo(equipo);
+            _equipos.Remove(equipo);
+        }
+
+        private void ValidarEquipoExistente(int index)
+        {
+            if (index == -1)
+                throw new Exception("Equipo no encontrado");
+        }
+
+        private void ValidarEquipoNoNulo(Equipo equipo)
+        {
             if (equipo == null)
                 throw new Exception("Equipo no encontrado");
-            _equipos.Remove(equipo);
         }
     }
 }
