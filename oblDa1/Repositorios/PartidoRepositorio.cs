f0e23e11517c61f6620c5d7385b15472d92eb793
Leandro <lc286847@fi365.ort.edu.uy>
2026-04-16 17:53:50 -0300

    refactor: extraer metodo ValidarPartidoExistente

--- oblDa1/Repositorios/PartidoRepositorio.cs
+++ oblDa1/Repositorios/PartidoRepositorio.cs
@@ -24,9 +24,14 @@ namespace Repositorios
         public void Actualizar(Partido partido)
         {
             var indice = _partidos.FindIndex(p => p.Id == partido.Id);
+            ValidarPartidoExistente(indice);
+            _partidos[indice] = partido;
+        }
+
+        private void ValidarPartidoExistente(int indice)
+        {
             if (indice == -1)
                 throw new Exception("Partido no encontrado");
-            _partidos[indice] = partido;
         }
     }
 }
