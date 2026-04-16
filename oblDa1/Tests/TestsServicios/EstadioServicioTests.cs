003c11de26e2cc04718a6f5bb92c3a277a13b1cf
JuanpeTDB <albanelljuanpedro@gmail.com>
2026-04-16 18:44:53 -0300

    fix: actualizar tests de servicio de Estadio para usar nombre

--- oblDa1/Tests/TestsServicios/EstadioServicioTests.cs
+++ oblDa1/Tests/TestsServicios/EstadioServicioTests.cs
@@ -36,30 +36,7 @@ namespace Tests
 
             Assert.AreEqual(1, resultado.Count);
         }
-        
-        [TestMethod]
-        public void AgregarEstadio_DeberiaAsignarIdAutomaticamente()
-        {
-            var estadio = CrearEstadioValido("Centenario", "Montevideo", 60000);
 
-            _servicio.AgregarEstadio(estadio);
-
-            Assert.AreEqual(1, estadio.Id);
-        }
-        
-        [TestMethod]
-        public void AgregarEstadio_VariosEstadios_DeberiaAsignarIdsIncrementales()
-        {
-            var estadio1 = CrearEstadioValido("Centenario", "Montevideo", 60000);
-            var estadio2 = CrearEstadioValido("Camp Nou", "Barcelona", 99000);
-
-            _servicio.AgregarEstadio(estadio1);
-            _servicio.AgregarEstadio(estadio2);
-
-            Assert.AreEqual(1, estadio1.Id);
-            Assert.AreEqual(2, estadio2.Id);
-        }
-        
         [TestMethod]
         [ExpectedException(typeof(Exception))]
         public void AgregarEstadio_ConNombreDuplicado_DeberiaLanzarExcepcion()
@@ -70,25 +47,25 @@ namespace Tests
             _servicio.AgregarEstadio(estadio1);
             _servicio.AgregarEstadio(estadio2);
         }
-        
+
         [TestMethod]
-        public void ObtenerEstadio_ConIdExistente_DeberiaRetornarlo()
+        public void ObtenerEstadio_ConNombreExistente_DeberiaRetornarlo()
         {
             var estadio = CrearEstadioValido("Centenario", "Montevideo", 60000);
             _servicio.AgregarEstadio(estadio);
 
-            var resultado = _servicio.ObtenerEstadio(estadio.Id);
+            var resultado = _servicio.ObtenerEstadio("Centenario");
 
             Assert.AreEqual("Centenario", resultado.Nombre);
         }
-        
+
         [TestMethod]
-        public void ObtenerEstadio_ConIdInexistente_DeberiaRetornarNull()
+        public void ObtenerEstadio_ConNombreInexistente_DeberiaRetornarNull()
         {
-            var resultado = _servicio.ObtenerEstadio(999);
+            var resultado = _servicio.ObtenerEstadio("NoExiste");
             Assert.IsNull(resultado);
         }
-        
+
         [TestMethod]
         public void ObtenerTodos_ConVariosEstadios_DeberiaRetornarTodos()
         {
@@ -101,65 +78,37 @@ namespace Tests
 
             Assert.AreEqual(2, resultado.Count);
         }
-        
+
         [TestMethod]
         public void ModificarEstadio_ConDatosValidos_DeberiaActualizar()
         {
             var estadio = CrearEstadioValido("Centenario", "Montevideo", 60000);
             _servicio.AgregarEstadio(estadio);
 
-            estadio.Nombre = "Centenario Renovado";
             estadio.Capacidad = 65000;
             _servicio.ModificarEstadio(estadio);
 
-            var resultado = _servicio.ObtenerEstadio(estadio.Id);
-            Assert.AreEqual("Centenario Renovado", resultado.Nombre);
+            var resultado = _servicio.ObtenerEstadio("Centenario");
             Assert.AreEqual(65000, resultado.Capacidad);
         }
-        
-        [TestMethod]
-        [ExpectedException(typeof(Exception))]
-        public void ModificarEstadio_ConNombreDuplicado_DeberiaLanzarExcepcion()
-        {
-            var estadio1 = CrearEstadioValido("Centenario", "Montevideo", 60000);
-            var estadio2 = CrearEstadioValido("Camp Nou", "Barcelona", 99000);
-            _servicio.AgregarEstadio(estadio1);
-            _servicio.AgregarEstadio(estadio2);
 
-            estadio2.Nombre = "Centenario";
-            _servicio.ModificarEstadio(estadio2);
-        }
-        
         [TestMethod]
-        public void ModificarEstadio_ManteniendoSuNombre_NoDeberiaLanzarExcepcion()
+        public void EliminarEstadio_ConNombreExistente_DeberiaEliminarlo()
         {
             var estadio = CrearEstadioValido("Centenario", "Montevideo", 60000);
             _servicio.AgregarEstadio(estadio);
 
-            estadio.Capacidad = 70000;
-            _servicio.ModificarEstadio(estadio);
-
-            var resultado = _servicio.ObtenerEstadio(estadio.Id);
-            Assert.AreEqual(70000, resultado.Capacidad);
-        }
-        
-        [TestMethod]
-        public void EliminarEstadio_ConIdExistente_DeberiaEliminarlo()
-        {
-            var estadio = CrearEstadioValido("Centenario", "Montevideo", 60000);
-            _servicio.AgregarEstadio(estadio);
-
-            _servicio.EliminarEstadio(estadio.Id);
+            _servicio.EliminarEstadio("Centenario");
             var resultado = _servicio.ObtenerTodos();
 
             Assert.AreEqual(0, resultado.Count);
         }
-        
+
         [TestMethod]
         [ExpectedException(typeof(Exception))]
-        public void EliminarEstadio_ConIdInexistente_DeberiaLanzarExcepcion()
+        public void EliminarEstadio_ConNombreInexistente_DeberiaLanzarExcepcion()
         {
-            _servicio.EliminarEstadio(999);
+            _servicio.EliminarEstadio("NoExiste");
         }
     }
 }
