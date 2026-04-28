f8ca65de99899085bc9871d3572b104e6fd30806
JuanpeTDB <albanelljuanpedro@gmail.com>
2026-04-28 17:34:30 -0300

    feat: importar equipo valido - variante facilidad comprension

---
+++ oblDa1/Tests/TestsServicios/ImportacionFacilidadComprensionServicioTests.cs
@@ -0,0 +1,31 @@
+using Dominio.Entidades;
+using Repositorios;
+using Servicios;
+
+namespace Tests.TestsServicios
+{
+    [TestClass]
+    public class ImportacionFacilidadComprensionServicioTests
+    {
+        private IEquipoRepositorio _equipoRepositorio = null!;
+        private IImportacionServicio _servicio = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _equipoRepositorio = new EquipoRepositorio();
+            _servicio = new ImportacionFacilidadComprensionServicio(_equipoRepositorio);
+        }
+
+        [TestMethod]
+        public void ImportarEquipos_ConUnEquipoValido_DeberiaImportarlo()
+        {
+            var csv = "Nombre,Confederación,RankingFIFA\nUruguay,CONMEBOL,1500";
+
+            var resultado = _servicio.ImportarEquipos(csv);
+
+            Assert.AreEqual(1, resultado.EquiposImportados);
+            Assert.AreEqual(0, resultado.Errores.Count);
+        }
+    }
+}
