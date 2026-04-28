24a5e24b46c7150da0c0f26334602c23a0314dee
JuanpeTDB <albanelljuanpedro@gmail.com>
2026-04-28 17:07:47 -0300

    feat: registrar error por confederacion invalida - variante bajo acoplamiento

--- oblDa1/Tests/TestsServicios/ImportacionBajoAcoplamientoServicioTests.cs
+++ oblDa1/Tests/TestsServicios/ImportacionBajoAcoplamientoServicioTests.cs
@@ -41,5 +41,16 @@ namespace Tests
             Assert.AreEqual(2, resultado.EquiposImportados);
             Assert.AreEqual(0, resultado.Errores.Count);
         }
+        
+        [TestMethod]
+        public void ImportarEquipos_ConConfederacionInvalida_DeberiaRegistrarError()
+        {
+            var csv = "Nombre,Confederación,RankingFIFA\nUruguay,INVALIDA,1500";
+
+            var resultado = _servicio.ImportarEquipos(csv);
+
+            Assert.AreEqual(0, resultado.EquiposImportados);
+            Assert.AreEqual(1, resultado.Errores.Count);
+        }
     }
 }
