412923c351408cb1b7279544cb1911cbc7ecc8af
Leandro <lc286847@fi365.ort.edu.uy>
2026-04-29 13:07:00 -0300

    test: [RED] verificar asignacion de SemillaFixture

---
+++ oblDa1/Tests/TestsDominio/FixtureTests.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Entidades;
+
+namespace Tests
+{
+    [TestClass]
+    public class FixtureTests
+    {
+        [TestMethod]
+        public void CrearFixture_ConSemillaValida_DeberiaAsignarSemilla()
+        {
+            var fixture = new Fixture();
+            fixture.SemillaFixture = 42;
+            Assert.AreEqual(42, fixture.SemillaFixture);
+        }
+    }
+}
