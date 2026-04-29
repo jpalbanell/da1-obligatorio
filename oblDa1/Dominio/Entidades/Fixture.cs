17fd7c8df1dbf6de8b1f5968c054eb73debfed81
Leandro <lc286847@fi365.ort.edu.uy>
2026-04-29 13:23:47 -0300

    feat: [GREEN] agregar MaxPartidosPorDia con valor predeterminado

--- oblDa1/Dominio/Entidades/Fixture.cs
+++ oblDa1/Dominio/Entidades/Fixture.cs
@@ -4,5 +4,6 @@
     {
         public int SemillaFixture { get; set; }
         public DateTime FechaInicioTorneo { get; set; } = new DateTime(2026, 6, 1);
+        public int MaxPartidosPorDia { get; set; } = 3;
     }
 }
