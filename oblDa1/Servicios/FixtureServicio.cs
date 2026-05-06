9e67980274ae1cc4e6e56a0f0edf1dc1212e8a4f
Leandro <lc286847@fi365.ort.edu.uy>
2026-05-06 13:47:52 -0300

    feat: [GREEN] asignar fechas respetando descanso mínimo por equipo

--- oblDa1/Servicios/FixtureServicio.cs
+++ oblDa1/Servicios/FixtureServicio.cs
@@ -199,42 +199,12 @@ namespace Servicios
         private void AsignarFechas(List<List<Partido>> todosLosPartidos, Fixture fixture)
         {
             var partidosPorDia = new Dictionary<DateTime, int>();
+            var ultimoPartidoPorEquipo = new Dictionary<string, DateTime>();
 
-            for (int jornada = 0; jornada < 3; jornada++)
-            {
-                var fechaJornada = fixture.FechaInicioTorneo.Date
-                    .AddDays(jornada * fixture.SeparacionEntreFechas);
-
-                var partidosDeJornada = new List<Partido>();
-
-                foreach (var partidosGrupo in todosLosPartidos)
-                {
-                    int inicioJornada = jornada * 2;
-                    partidosDeJornada.Add(partidosGrupo[inicioJornada]);
-                    partidosDeJornada.Add(partidosGrupo[inicioJornada + 1]);
-                }
-
-                var fechaActual = fechaJornada;
-
-                foreach (var partido in partidosDeJornada)
-                {
-                    while (true)
-                    {
-                        if (!partidosPorDia.ContainsKey(fechaActual))
-                            partidosPorDia[fechaActual] = 0;
-
-                        if (partidosPorDia[fechaActual] < fixture.MaxPartidosPorDia)
-                            break;
-
-                        fechaActual = fechaActual.AddDays(1);
-                    }
-
-                    int turno = partidosPorDia[fechaActual];
-                    partido.Fecha = fechaActual.AddHours(14 + (turno * 4));
-                    partidosPorDia[fechaActual]++;
-                }
-            }
+            AsignarFechasJornadasPrevias(todosLosPartidos, fixture, partidosPorDia, ultimoPartidoPorEquipo);
+            AsignarFechasUltimaJornada(todosLosPartidos, fixture, partidosPorDia, ultimoPartidoPorEquipo);
         }
+        
         
         private void AsignarEstadios()
         {
@@ -248,6 +218,132 @@ namespace Servicios
             {
                 partidos[i].Estadio = estadios[i % estadios.Count];
             }
+        }
+        
+        private void AsignarFechasJornadasPrevias(
+            List<List<Partido>> todosLosPartidos,
+            Fixture fixture,
+            Dictionary<DateTime, int> partidosPorDia,
+            Dictionary<string, DateTime> ultimoPartidoPorEquipo)
+        {
+            var partidosJornadasPrevias = new List<Partido>();
+
+            foreach (var partidosGrupo in todosLosPartidos)
+            {
+                partidosJornadasPrevias.Add(partidosGrupo[0]);
+                partidosJornadasPrevias.Add(partidosGrupo[1]);
+                partidosJornadasPrevias.Add(partidosGrupo[2]);
+                partidosJornadasPrevias.Add(partidosGrupo[3]);
+            }
+
+            var fechaActual = fixture.FechaInicioTorneo.Date;
+
+            foreach (var partido in partidosJornadasPrevias)
+            {
+                fechaActual = BuscarFechaDisponible(
+                    fechaActual, partido, fixture, partidosPorDia, ultimoPartidoPorEquipo);
+
+                int turno = partidosPorDia.ContainsKey(fechaActual) ? partidosPorDia[fechaActual] : 0;
+                partido.Fecha = fechaActual.AddHours(14 + (turno * 4));
+
+                ActualizarContadores(fechaActual, partido, partidosPorDia, ultimoPartidoPorEquipo);
+            }
+        }
+        
+        private void AsignarFechasUltimaJornada(
+            List<List<Partido>> todosLosPartidos,
+            Fixture fixture,
+            Dictionary<DateTime, int> partidosPorDia,
+            Dictionary<string, DateTime> ultimoPartidoPorEquipo)
+        {
+            foreach (var partidosGrupo in todosLosPartidos)
+            {
+                var partidoA = partidosGrupo[4];
+                var partidoB = partidosGrupo[5];
+
+                var fechaBase = fixture.FechaInicioTorneo.Date;
+                var fechaActual = fechaBase;
+
+                while (true)
+                {
+                    if (!partidosPorDia.ContainsKey(fechaActual))
+                        partidosPorDia[fechaActual] = 0;
+
+                    bool caben = partidosPorDia[fechaActual] + 2 <= fixture.MaxPartidosPorDia;
+                    bool equiposDescansados = EquiposDescansaron(fechaActual, partidoA, fixture, ultimoPartidoPorEquipo)
+                                              && EquiposDescansaron(fechaActual, partidoB, fixture, ultimoPartidoPorEquipo);
+
+                    if (caben && equiposDescansados)
+                        break;
+
+                    fechaActual = fechaActual.AddDays(1);
+                }
+
+                partidoA.Fecha = fechaActual.AddHours(14);
+                partidoB.Fecha = fechaActual.AddHours(14);
+
+                ActualizarContadores(fechaActual, partidoA, partidosPorDia, ultimoPartidoPorEquipo);
+                ActualizarContadores(fechaActual, partidoB, partidosPorDia, ultimoPartidoPorEquipo);
+            }
+        }
+        
+        private DateTime BuscarFechaDisponible(
+            DateTime fechaDesde,
+            Partido partido,
+            Fixture fixture,
+            Dictionary<DateTime, int> partidosPorDia,
+            Dictionary<string, DateTime> ultimoPartidoPorEquipo)
+        {
+            var fechaActual = fechaDesde;
+
+            while (true)
+            {
+                if (!partidosPorDia.ContainsKey(fechaActual))
+                    partidosPorDia[fechaActual] = 0;
+
+                bool hayHueco = partidosPorDia[fechaActual] < fixture.MaxPartidosPorDia;
+                bool equiposDescansados = EquiposDescansaron(fechaActual, partido, fixture, ultimoPartidoPorEquipo);
+
+                if (hayHueco && equiposDescansados)
+                    return fechaActual;
+
+                fechaActual = fechaActual.AddDays(1);
+            }
+        }
+        
+        private bool EquiposDescansaron(
+            DateTime fecha,
+            Partido partido,
+            Fixture fixture,
+            Dictionary<string, DateTime> ultimoPartidoPorEquipo)
+        {
+            if (ultimoPartidoPorEquipo.TryGetValue(partido.EquipoLocal.Nombre, out var ultimoLocal))
+            {
+                if ((fecha - ultimoLocal.Date).Days < fixture.SeparacionEntreFechas)
+                    return false;
+            }
+
+            if (ultimoPartidoPorEquipo.TryGetValue(partido.EquipoVisitante.Nombre, out var ultimoVisitante))
+            {
+                if ((fecha - ultimoVisitante.Date).Days < fixture.SeparacionEntreFechas)
+                    return false;
+            }
+
+            return true;
+        }
+        
+        private void ActualizarContadores(
+            DateTime fecha,
+            Partido partido,
+            Dictionary<DateTime, int> partidosPorDia,
+            Dictionary<string, DateTime> ultimoPartidoPorEquipo)
+        {
+            if (!partidosPorDia.ContainsKey(fecha))
+                partidosPorDia[fecha] = 0;
+
+            partidosPorDia[fecha]++;
+            ultimoPartidoPorEquipo[partido.EquipoLocal.Nombre] = fecha;
+            ultimoPartidoPorEquipo[partido.EquipoVisitante.Nombre] = fecha;
         }
         
         private string Normalizar(string texto)
