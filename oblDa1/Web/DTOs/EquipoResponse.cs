21bdbc75f06f03f93f1b853c9b2736ea4427c2b5
Leandro <lc286847@fi365.ort.edu.uy>
2026-05-07 10:42:51 -0300

    feat: pantalla Equipos con DTOs y generador automático

---
+++ oblDa1/Web/DTOs/EquipoResponse.cs
@@ -0,0 +1,18 @@
+﻿using Dominio.Entidades;
+
+namespace Web.DTOs
+{
+    public class EquipoResponse
+    {
+        public string Nombre { get; set; } = "";
+        public string Confederacion { get; set; } = "";
+        public int RankingFifa { get; set; }
+
+        public static EquipoResponse FromEntity(Equipo equipo) => new()
+        {
+            Nombre = equipo.Nombre,
+            Confederacion = equipo.Confederacion.ToString(),
+            RankingFifa = equipo.RankingFifa
+        };
+    }
+}
