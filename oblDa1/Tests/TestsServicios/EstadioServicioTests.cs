0201a45c78d50641e23911d974d75360c898ff26
Leandro <lc286847@fi365.ort.edu.uy>
2026-05-11 16:33:16 -0300

    fix: validar existencia de estadio antes de eliminar

--- oblDa1/Tests/TestsServicios/EstadioServicioTests.cs
+++ oblDa1/Tests/TestsServicios/EstadioServicioTests.cs
@@ -235,5 +235,12 @@ namespace Tests
 
             _servicio.EliminarEstadio("Centenario");
         }
+        
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EliminarEstadio_EstadioInexistente_DeberiaLanzarExcepcion()
+        {
+            _servicio.EliminarEstadio("EstadioQueNoExiste");
+        }
     }
 }
