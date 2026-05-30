f13a2a58ff2f21c3a41258b1da4df65cece05721
Leandro <lc286847@fi365.ort.edu.uy>
2026-05-30 18:52:22 -0300

    feat: agregar ConfederacionExtensiones con CupoMaximo como extension method

---
+++ oblDa1/Dominio/Entidades/ConfederacionExtenciones.cs
@@ -0,0 +1,22 @@
+﻿namespace Dominio.Entidades
+{
+    public static class ConfederacionExtensiones
+    {
+        private static readonly Dictionary<Confederacion, int> Cupos = new()
+        {
+            { Confederacion.UEFA, 16 },
+            { Confederacion.CONMEBOL, 7 },
+            { Confederacion.CONCACAF, 7 },
+            { Confederacion.CAF, 9 },
+            { Confederacion.AFC, 8 },
+            { Confederacion.OFC, 1 }
+        };
+
+        public static int CupoMaximo(this Confederacion confederacion)
+        {
+            if (Cupos.TryGetValue(confederacion, out var cupo))
+                return cupo;
+            throw new ArgumentException($"Confederación inválida: {confederacion}");
+        }
+    }
+}
