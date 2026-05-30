74a667e5182dbbc029499315d77a61aa43ff9ee6
Leandro <lc286847@fi365.ort.edu.uy>
2026-05-30 19:07:03 -0300

    feat: Barajar con misma semilla produce mismo resultado

---
+++ oblDa1/Dominio/Entidades/BarajadorDeterminisitico.cs
@@ -0,0 +1,14 @@
+﻿namespace Dominio
+{
+    public static class BarajadorDeterministico
+    {
+        public static void Barajar<T>(List<T> lista, Random random)
+        {
+            for (int i = lista.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i + 1);
+                (lista[i], lista[j]) = (lista[j], lista[i]);
+            }
+        }
+    }
+}
---
+++ oblDa1/Tests/TestsDominio/BarajadorDeterministicoTests.cs
@@ -0,0 +1,21 @@
+﻿using Dominio;
+
+namespace Tests
+{
+    [TestClass]
+    public class BarajadorDeterministicoTests
+    {
+        [TestMethod]
+        public void Barajar_MismaSemilla_ProduceMismoResultado()
+        {
+            var lista1 = new List<int> { 1, 2, 3, 4, 5 };
+            var lista2 = new List<int> { 1, 2, 3, 4, 5 };
+
+            BarajadorDeterministico.Barajar(lista1, new Random(42));
+            BarajadorDeterministico.Barajar(lista2, new Random(42));
+
+            CollectionAssert.AreEqual(lista1, lista2);
+            CollectionAssert.AreNotEqual(lista1, new List<int> { 1, 2, 3, 4, 5 });
+        }
+    }
+}
