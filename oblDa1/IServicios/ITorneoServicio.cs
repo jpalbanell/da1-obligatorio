38f1a55f1cacc4aa572af2ef8206798daba177db
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-05-31 17:01:44 -0300

    feat: crear TorneoServicio e ITorneoServicio vacios

---
+++ oblDa1/IServicios/ITorneoServicio.cs
@@ -0,0 +1,8 @@
+using Dominio.Entidades;
+
+namespace IServicios
+{
+    public interface ITorneoServicio
+    {
+    }
+}
---
+++ oblDa1/Servicios/TorneoServicio.cs
@@ -0,0 +1,36 @@
+using Dominio.Entidades;
+using Dominio;
+using IRepositorios;
+using IServicios;
+
+namespace Servicios
+{
+    public class TorneoServicio : ITorneoServicio
+    {
+        private readonly IEquipoRepositorio _equipoRepositorio;
+        private readonly IEstadioRepositorio _estadioRepositorio;
+        private readonly IPartidoRepositorio _partidoRepositorio;
+        private readonly IGrupoRepositorio _grupoRepositorio;
+        private readonly IFixtureRepositorio _fixtureRepositorio;
+        private readonly IAuditoriaServicio _auditoriaServicio;
+        private readonly ISesionServicio _sesionServicio;
+
+        public TorneoServicio(
+            IEquipoRepositorio equipoRepositorio,
+            IEstadioRepositorio estadioRepositorio,
+            IPartidoRepositorio partidoRepositorio,
+            IGrupoRepositorio grupoRepositorio,
+            IFixtureRepositorio fixtureRepositorio,
+            IAuditoriaServicio auditoriaServicio,
+            ISesionServicio sesionServicio)
+        {
+            _equipoRepositorio = equipoRepositorio;
+            _estadioRepositorio = estadioRepositorio;
+            _partidoRepositorio = partidoRepositorio;
+            _grupoRepositorio = grupoRepositorio;
+            _fixtureRepositorio = fixtureRepositorio;
+            _auditoriaServicio = auditoriaServicio;
+            _sesionServicio = sesionServicio;
+        }
+    }
+}
