928d1d65ae7d8ecd5d2ec7dbd3eba3eb044d839d
SantiagoJorcin4 <sj325511@fi365.ort.edu.uy>
2026-06-04 13:31:24 -0300

    feat: agregar propiedad Id a Fixture para persistencia EF

--- oblDa1/Dominio/Entidades/Fixture.cs
+++ oblDa1/Dominio/Entidades/Fixture.cs
@@ -2,6 +2,7 @@
 {
     public class Fixture
     {
+        public int Id { get; set; }
         public int SemillaFixture { get; set; }
         public DateTime FechaInicioTorneo { get; set; } = new DateTime(2026, 6, 1);
         public int MaxPartidosPorDia { get; set; } = 3;
