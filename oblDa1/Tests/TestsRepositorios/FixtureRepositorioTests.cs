d5d3b264405e886c76f96a2665878bf717fa3acb
JuanpeTDB <albanelljuanpedro@gmail.com>
2026-06-16 10:08:57 -0300

    test: cubrir rama de actualizacion de fixture existente en FixtureRepositorio

--- oblDa1/Tests/TestsRepositorios/FixtureRepositorioTests.cs
+++ oblDa1/Tests/TestsRepositorios/FixtureRepositorioTests.cs
@@ -21,6 +21,24 @@ namespace Tests
             _repositorio = new FixtureRepositorio(_context);
         }
 
+        private Equipo CrearEquipoValido(string nombre)
+        {
+            var equipo = new Equipo();
+            equipo.Nombre = nombre;
+            equipo.RankingFifa = 1000;
+            equipo.Confederacion = Confederacion.CONMEBOL;
+            return equipo;
+        }
+
+        private Estadio CrearEstadioValido(string nombre)
+        {
+            var estadio = new Estadio();
+            estadio.Nombre = nombre;
+            estadio.Ciudad = "Ciudad Test";
+            estadio.Capacidad = 50000;
+            return estadio;
+        }
+
         [TestMethod]
         public void Guardar_FixtureNuevo_LoInserta()
         {
@@ -66,6 +84,119 @@ namespace Tests
 
             Assert.IsNotNull(resultado);
             Assert.AreEqual(42, resultado.SemillaFixture);
+        }
+
+        [TestMethod]
+        public void Guardar_FixtureExistente_ActualizaTodasLasPropiedadesEscalares()
+        {
+            var fixtureInicial = new Fixture();
+            fixtureInicial.SemillaFixture = 42;
+            _repositorio.Guardar(fixtureInicial);
+
+            var fixtureActualizado = new Fixture();
+            fixtureActualizado.SemillaFixture = 99;
+            fixtureActualizado.EstaGenerado = true;
+            fixtureActualizado.CrucesGenerados = true;
+            fixtureActualizado.MaxPartidosPorDia = 5;
+            fixtureActualizado.SeparacionEntreFechas = 5;
+            fixtureActualizado.NombreMotorSimulacion = "Personalizado";
+            fixtureActualizado.FechaInicioTorneo = new DateTime(2027, 1, 1);
+            _repositorio.Guardar(fixtureActualizado);
+
+            var resultado = _repositorio.Obtener();
+            Assert.AreEqual(99, resultado.SemillaFixture);
+            Assert.IsTrue(resultado.EstaGenerado);
+            Assert.IsTrue(resultado.CrucesGenerados);
+            Assert.AreEqual(5, resultado.MaxPartidosPorDia);
+            Assert.AreEqual(5, resultado.SeparacionEntreFechas);
+            Assert.AreEqual("Personalizado", resultado.NombreMotorSimulacion);
+            Assert.AreEqual(new DateTime(2027, 1, 1), resultado.FechaInicioTorneo);
+        }
+
+        [TestMethod]
+        public void Guardar_FixtureExistenteConMenosEquipos_SincronizaEquiposEnBD()
+        {
+            var equipo1 = CrearEquipoValido("Argentina");
+            var equipo2 = CrearEquipoValido("Brasil");
+            _context.Equipos.AddRange(equipo1, equipo2);
+            _context.SaveChanges();
+
+            var fixtureInicial = new Fixture();
+            fixtureInicial.Equipos.Add(equipo1);
+            fixtureInicial.Equipos.Add(equipo2);
+            _repositorio.Guardar(fixtureInicial);
+
+            var fixtureActualizado = new Fixture();
+            fixtureActualizado.Equipos.Add(equipo1);
+            _repositorio.Guardar(fixtureActualizado);
+
+            var resultado = _repositorio.Obtener();
+            Assert.AreEqual(1, resultado.Equipos.Count);
+            Assert.AreEqual("Argentina", resultado.Equipos[0].Nombre);
+        }
+
+        [TestMethod]
+        public void Guardar_FixtureExistenteConEquipoSinPersistir_DescartaEquipoSilenciosamente()
+        {
+            var equipoReal = CrearEquipoValido("Argentina");
+            _context.Equipos.Add(equipoReal);
+            _context.SaveChanges();
+
+            var fixtureInicial = new Fixture();
+            _repositorio.Guardar(fixtureInicial);
+
+            var equipoFantasma = CrearEquipoValido("EquipoFantasma");
+            var fixtureActualizado = new Fixture();
+            fixtureActualizado.Equipos.Add(equipoReal);
+            fixtureActualizado.Equipos.Add(equipoFantasma);
+            _repositorio.Guardar(fixtureActualizado);
+
+            var resultado = _repositorio.Obtener();
+            Assert.AreEqual(1, resultado.Equipos.Count);
+            Assert.AreEqual("Argentina", resultado.Equipos[0].Nombre);
+        }
+
+        [TestMethod]
+        public void Guardar_FixtureExistenteConMenosEstadios_SincronizaEstadiosEnBD()
+        {
+            var estadio1 = CrearEstadioValido("Estadio A");
+            var estadio2 = CrearEstadioValido("Estadio B");
+            _context.Estadios.AddRange(estadio1, estadio2);
+            _context.SaveChanges();
+
+            var fixtureInicial = new Fixture();
+            fixtureInicial.Estadios.Add(estadio1);
+            fixtureInicial.Estadios.Add(estadio2);
+            _repositorio.Guardar(fixtureInicial);
+
+            var fixtureActualizado = new Fixture();
+            fixtureActualizado.Estadios.Add(estadio1);
+            _repositorio.Guardar(fixtureActualizado);
+
+            var resultado = _repositorio.Obtener();
+            Assert.AreEqual(1, resultado.Estadios.Count);
+            Assert.AreEqual("Estadio A", resultado.Estadios[0].Nombre);
+        }
+
+        [TestMethod]
+        public void Guardar_FixtureExistenteConEstadioSinPersistir_DescartaEstadioSilenciosamente()
+        {
+            var estadioReal = CrearEstadioValido("Estadio A");
+            _context.Estadios.Add(estadioReal);
+            _context.SaveChanges();
+
+            var fixtureInicial = new Fixture();
+            _repositorio.Guardar(fixtureInicial);
+
+            var estadioFantasma = CrearEstadioValido("Estadio Fantasma");
+            var fixtureActualizado = new Fixture();
+            fixtureActualizado.Estadios.Add(estadioReal);
+            fixtureActualizado.Estadios.Add(estadioFantasma);
+            _repositorio.Guardar(fixtureActualizado);
+
+            var resultado = _repositorio.Obtener();
+            Assert.AreEqual(1, resultado.Estadios.Count);
+            Assert.AreEqual("Estadio A", resultado.Estadios[0].Nombre);
         }
     }
 }
